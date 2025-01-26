66492e5fe9e9bea0aa2e3f3ef945bc11f7eb91fb
Katerina <ekaterina041117@gmail.com>
2025-01-26 03:45:01 +0200

    C:/Program Files/Git/

--- src/utils/getEnvVar.js
+++ src/utils/getEnvVar.js
@@ -9,5 +9,5 @@ export const getEnvVar = (name, defaultValue) => {
 
     if (defaultValue) return defaultValue;
 
-    throw new Error(`Missing: process.env['${name}'] enviroment variable`);
+    throw new Error(`Missing: process.env['${name}'].`);
 };
